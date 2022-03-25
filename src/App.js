21d84c20c7211d21d83657a653a139530357052b
MyoMyintAung <myomyintaung064@gmail.com>
2022-03-25 12:57:13 +0630

    added Transation & AddTransation components

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import "./App.css";
 import Header from "./components/Header";
 import Balance from "./components/Balance.js";
 import IncomeExpenses from "./components/IncomeExpenses";
+import TransationList from "./components/TransationList";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Header />
       <div>
         <Balance />
         <IncomeExpenses />
+        <TransationList />
       </div>
-    </>
+    </div>
   );
 }
 
