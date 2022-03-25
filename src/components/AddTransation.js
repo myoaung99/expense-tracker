35fd76a4d30e3b70ba240d545f748b70f2a614f3
MyoMyintAung <myomyintaung064@gmail.com>
2022-03-25 13:09:57 +0630

    added component level state to AddTransation Component

--- src/components/AddTransation.js
+++ src/components/AddTransation.js
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddTransation = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const textChangeHandler = (event) => {
+    setText(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setAmount(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
   return (
     <>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" id="text" placeholder="Enter text..." />
+          <input
+            value={text}
+            type="text"
+            id="text"
+            placeholder="Enter text..."
+            onChange={textChangeHandler}
+          />
         </div>
         <div className="form-control">
           <label htmlFor="amount">
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number" id="amount" placeholder="Enter amount..." />
+          <input
+            value={amount}
+            type="number"
+            id="amount"
+            placeholder="Enter amount..."
+            onChange={amountChangeHandler}
+          />
         </div>
         <button className="btn">Add transaction</button>
       </form>
